a660321445b04150333bf16a8886c755474ce752
Carson <carsonjohnbain@gmail.com>
2024-01-23 22:01:12 -0800

    getting basic modal layout

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+
+};
+
+// HINTS
+// USE PROMPT 
+// USE NEW KEYWORD TO CREATE A NEW OBJECT
