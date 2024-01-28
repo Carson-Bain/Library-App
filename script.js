6fa70f8c6ae428f8db5a966bdc3665d24415d935
Carson <carsonjohnbain@gmail.com>
2024-01-27 19:30:08 -0800

    delete button and everything else should work

--- script.js
+++ script.js
@@ -3,20 +3,17 @@ const addBookBtn = document.querySelector('#add-book');
 const modal = document.querySelector('#modal');
 const cancelBtn = document.querySelector('#cancel')
 const submitBtn = document.querySelector('#submit');
-const readBtn = document.querySelector('#read-button');
+const readBtn = document.createElement('button');
+const deleteBtn = document.createElement('button');
 
 // ELEMENTS
 const cardContainer = document.querySelector('.card-container');
 const card = document.querySelector('.card');
 
-
-
-submitBtn.addEventListener('click', (e) => {
+submitBtn.addEventListener('click', function(e) {
     e.preventDefault();
-   const newDiv = document.createElement('div');
-   newDiv.classList.add('card');
-   cardContainer.appendChild(newDiv);
-   modal.close();
+    addBookToLibrary();
+    modal.close();
 })
 
 addBookBtn.addEventListener('click', () => {
@@ -27,9 +24,10 @@ cancelBtn.addEventListener('click', () => {
     modal.close();
 })
 
-readBtn.addEventListener('click', () => {
-    readBtn.classList.toggle('not-read');  
-})
+// readBtn.addEventListener('click', () => {
+//     console.log('error');
+//     readBtn.classList.toggle('not-read');  
+// })
 
 const myLibrary = [];
 
@@ -41,8 +39,49 @@ function book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#page-count').value;
+    const read = document.querySelector('#read').value;
+    let newBook = new book(title, author, pages, read);
+    myLibrary.push(newBook);
+    renderCard();
 };
 
-// HINTS
-// USE NEW KEYWORD TO CREATE A NEW OBJECT
+function renderCard() {
+    const cardContainer = document.querySelector('.card-container');
+    cardContainer.innerHTML = '';
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        cardContainer.appendChild(cardDiv);
+        const titleDiv = document.createElement('div');
+        titleDiv.textContent = `Title: ${book.title}`;
+        cardDiv.appendChild(titleDiv);
+        const authorDiv = document.createElement('div');
+        authorDiv.textContent = `Author: ${book.author}`;
+        cardDiv.appendChild(authorDiv);
+        const pagesDiv = document.createElement('div');
+        pagesDiv.textContent = `Pages: ${book.pages}`
+        cardDiv.appendChild(pagesDiv);
+        const readBtn = document.createElement('button');
+        const deleteBtn = document.createElement('button');
+        readBtn.classList.add('has-read');
+        readBtn.textContent = 'Read';
+        cardDiv.appendChild(readBtn);
+        deleteBtn.classList.add('delete');
+        deleteBtn.textContent = 'Delete';
+        cardDiv.appendChild(deleteBtn);
+        
+        readBtn.addEventListener('click', () => {
+            console.log('error');
+            readBtn.classList.toggle('not-read');  
+        })
+        
+        deleteBtn.addEventListener('click', () => {
+            myLibrary.splice([i], 1);
+            cardContainer.removeChild(cardDiv);
+        })
+    }
+}
